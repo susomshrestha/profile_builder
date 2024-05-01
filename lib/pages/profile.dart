f395f3e8b4e04236f23964fcb6f95dc5cc01f58f
Susom <susomshrestha@gmail.com>
2024-05-01 15:36:33 -0400

    remove margins for the user bio card

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -20,135 +20,153 @@ class Profile extends StatelessWidget {
       body: Stack(
         fit: StackFit.expand,
         children: [
-          Expanded(
-            // flex: 8,
-            child: user.image.path == ''
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 140,
+            child: user.image.path.isEmpty
                 ? Image.asset(
                     'assets/images/profile.jpg',
                     fit: BoxFit.fill,
                   )
                 : Image.file(user.image, fit: BoxFit.cover),
           ),
-          Positioned(
-            bottom: 140,
-            left: 0,
-            right: 0,
-            height: 150,
-            child: Container(
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                gradient: LinearGradient(colors: [
-                  Colors.black.withOpacity(0.7),
-                  Colors.black.withOpacity(0.6),
-                  Colors.black.withOpacity(0),
-                ], begin: Alignment.bottomCenter, end: Alignment.topCenter),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '${user.name}, ${user.age}',
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w900,
-                        fontSize: 20),
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              const Icon(
-                                Icons.work,
-                                color: Colors.white,
-                              ),
-                              const SizedBox(
-                                width: 10,
-                              ),
-                              Text(
-                                user.occupation,
-                                style: const TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                                color: Theme.of(context).colorScheme.primary,
-                                borderRadius: const BorderRadius.all(
-                                    Radius.circular(20))),
-                            padding: const EdgeInsets.all(4),
-                            child: const Icon(
-                              Icons.favorite_rounded,
-                              color: Colors.white,
-                              size: 24,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Container(
-                        decoration: ShapeDecoration(
-                            color: Theme.of(context).colorScheme.primary,
-                            shape:
-                                const StarBorder.polygon(pointRounding: 0.4)),
-                        padding: const EdgeInsets.all(8),
-                        child: const Icon(
-                          Icons.star_rate_rounded,
-                          color: Colors.white,
-                          size: 32,
-                        ),
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            ),
+          _buildUserInfo(context),
+          _buildUserBio(context)
+        ],
+      ),
+    );
+  }
+
+  Widget _buildUserBio(BuildContext context) {
+    return Positioned(
+      bottom: 0,
+      left: 0,
+      right: 0,
+      height: 150,
+      child: Card(
+        margin: EdgeInsets.zero,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.zero,
+            bottomRight: Radius.zero,
+            topLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
           ),
-          Positioned(
-            height: 150,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Card(
-              child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      height: 60, // Set a fixed height for the bio
-                      child: SingleChildScrollView(
-                        child: Text(
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Theme.of(context).colorScheme.primary),
-                          user.bio,
-                          overflow: TextOverflow
-                              .clip, // Clip the text if it's too long
-                        ),
-                      ),
-                    ),
-                    CustomPreferences(preferences: user.preferences),
-                  ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                height: 60, // Set a fixed height for the bio
+                child: SingleChildScrollView(
+                  child: Text(
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.primary),
+                    user.bio,
+                    overflow:
+                        TextOverflow.clip, // Clip the text if it's too long
+                  ),
                 ),
               ),
+              CustomPreferences(preferences: user.preferences),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildUserInfo(BuildContext context) {
+    return Positioned(
+      bottom: 140,
+      left: 0,
+      right: 0,
+      height: 150,
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Colors.black.withOpacity(0.7),
+            Colors.black.withOpacity(0.6),
+            Colors.black.withOpacity(0),
+          ], begin: Alignment.bottomCenter, end: Alignment.topCenter),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '${user.name}, ${user.age}',
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w900,
+                  fontSize: 20),
             ),
-          )
-        ],
+            const SizedBox(
+              height: 4,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Icon(
+                          Icons.work,
+                          color: Colors.white,
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          user.occupation,
+                          style: const TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).colorScheme.primary,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(20))),
+                      padding: const EdgeInsets.all(4),
+                      child: const Icon(
+                        Icons.favorite_rounded,
+                        color: Colors.white,
+                        size: 24,
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  decoration: ShapeDecoration(
+                      color: Theme.of(context).colorScheme.primary,
+                      shape: const StarBorder.polygon(pointRounding: 0.4)),
+                  padding: const EdgeInsets.all(8),
+                  child: const Icon(
+                    Icons.star_rate_rounded,
+                    color: Colors.white,
+                    size: 32,
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
